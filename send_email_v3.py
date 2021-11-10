3757fc02239585f7d135e7a19e32c66b8c739dfb
Arthur Patricio <arthurpatricio@hotmail.com>
2021-11-10 09:51:36 -0300

    Update send_email_v3.py

--- send_email_v3.py
+++ send_email_v3.py
@@ -6,43 +6,52 @@ from email.message import EmailMessage
 
 
 def send_email():
+    
+    # SPREADSHEET DATA SOURCE
+    df = pd.read_excel('spreadsheet.xlsx', sheet_name='tab name')
 
-    df = pd.read_excel('xxx')
+    today_plus_10 = datetime.today() + timedelta(days=10)
+    today_plus_10 = today_plus_10.date()
+    today = datetime.today()
+    today = today.date()
 
     for index, row in df.iterrows():
-        pandas_timestamp =row['Valid Until']
-        license_date = pandas_timestamp.to_pydatetime()
-        license_date = license_date.date()
-        today = datetime.today() + timedelta(days=10)
-        today = today.date()
-        delta = today - license_date
-        if delta.days > 0:
-            
-            email_list = ['xxx']
+        expiration_date_timestamp =row['Valid Until']
+        expiration_date = expiration_date_timestamp.to_pydatetime()
+        expiration_date = expiration_date.date()
+        activation_date_timestamp =row['Activation Date']
+        activation_date = activation_date_timestamp.to_pydatetime()
+        activation_date = activation_date.date()
+        delta = today_plus_10 - expiration_date
+
+
+        if delta.days > 0 and expiration_date.day - today.day > 0 and str(row['Acknowledgment']).lower() == 'nan':
+
+            # LIST OF EMAILS TO SEND TO
+            email_list = ['email address 1, email address 2, ...']
 
             msg = EmailMessage()
-            msg['Subject'] = f'A LICENÇA DO CLIENTE {row["Client"]} ESTÁ PARA EXPIRAR'
-            msg['From'] = 'xxx'
+            msg['Subject'] = f'A LICENÇA DO CLIENTE {row["Client"]} ({row["Environment"]}) ESTÁ PARA EXPIRAR (EM {expiration_date.day - today.day} DIAS)'
+            # EMAIL ADDRESS THAT SENDS THE EMAIL
+            msg['From'] = 'email address that sends the email'
             msg['To'] = email_list
-            msg.set_content(f'A licença do cliente {row["Client"]} vai expirar em: {delta.days} dias')
+            msg.set_content(f'Uma licença do cliente {row["Client"]} de irá expirar em {expiration_date} \n\n \
+Ambiente: {row["Environment"]} \n\n \
+Natureza da Licença: {row["Subject"]} \n\n \
+Data de Ativação: {activation_date} \n\n \
+Número Serial: {row["Serial Number"]} \n\n \
+Chave: {row["Key"]}')
 
             with smtplib.SMTP('smtp.outlook.com', 587) as smtp:
                 smtp.ehlo()
                 smtp.starttls()
                 smtp.ehlo()
-
-                #smtp.login(EMAIL, PASSWORD)
-                smtp.login('xxx', 'xxx')
-
-                #subject ='PYTHON EMAIL SENDER TEST'
-                #body = 'TESTING ON HOW TO SEND EMAILS WITH PYTHON!!'
-
-                #msg = f'Subject: {subject}\n\n{body}'
-
-                #smtp.sendmail('support@netconamericas.com', email_list, msg)
+                
+                # EMAIL LOGIN
+                smtp.login('email address for login', 'email password for login')
 
                 smtp.send_message(msg)
-            print('A licença do cliente', row['Client'],'vai expirar em:' , delta.days, 'dias')
+            #print('A licença do cliente', row['Client'],'vai expirar em' , expiration_date)
 
 
 if __name__ == '__main__':
